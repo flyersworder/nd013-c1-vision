0512a1e7afe9be5541caf485d7aa6b0ccef111c7
flyersworder <yeqi519@gmail.com>
2022-10-03 22:22:30 +0200

    resolved some issues

--- experiments/exporter_main_v2.py
+++ experiments/exporter_main_v2.py
@@ -95,7 +95,7 @@ If side inputs are desired, the following arguments could be appended
 from absl import app
 from absl import flags
 
-import tensorflow.compat.v2 as tf
+import tensorflow as tf
 from google.protobuf import text_format
 from object_detection import exporter_lib_v2
 from object_detection.protos import pipeline_pb2
